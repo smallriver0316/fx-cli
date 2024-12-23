be963102385bf0ab23c1e240d32e1034c1640f62
smallriver0316 <stream0316@gmail.com>
2024-12-23 17:33:39 +0900

    Replaced unordered_map with std::array for currency ma table

--- src/Currency.cpp
+++ src/Currency.cpp
@@ -1,71 +1,83 @@
 #include <algorithm>
-#include <unordered_map>
+#include <array>
 
 #include "fx/Currency.hpp"
 
-const std::unordered_map<std::string, CurrencyPair> currency_pair_map = {
-    {"USDJPY", CurrencyPair::USDJPY},
-    {"EURJPY", CurrencyPair::EURJPY},
-    {"EURUSD", CurrencyPair::EURUSD},
-    {"AUDCHF", CurrencyPair::AUDCHF},
-    {"AUDJPY", CurrencyPair::AUDJPY},
-    {"AUDNZD", CurrencyPair::AUDNZD},
-    {"AUDUSD", CurrencyPair::AUDUSD},
-    {"CADJPY", CurrencyPair::CADJPY},
-    {"CHFJPY", CurrencyPair::CHFJPY},
-    {"EURAUD", CurrencyPair::EURAUD},
-    {"EURCAD", CurrencyPair::EURCAD},
-    {"EURCHF", CurrencyPair::EURCHF},
-    {"EURGBP", CurrencyPair::EURGBP},
-    {"EURNZD", CurrencyPair::EURNZD},
-    {"GBPAUD", CurrencyPair::GBPAUD},
-    {"GBPCHF", CurrencyPair::GBPCHF},
-    {"GBPJPY", CurrencyPair::GBPJPY},
-    {"GBPNZD", CurrencyPair::GBPNZD},
-    {"GBPUSD", CurrencyPair::GBPUSD},
-    {"NZDJPY", CurrencyPair::NZDJPY},
-    {"NZDUSD", CurrencyPair::NZDUSD},
-    {"USDCAD", CurrencyPair::USDCAD},
-    {"USDCHF", CurrencyPair::USDCHF},
-    {"ZARJPY", CurrencyPair::ZARJPY}};
+const std::array<std::pair<std::string, CurrencyPair>, 24> currency_pair_map = {{{"USDJPY", CurrencyPair::USDJPY},
+                                                                                 {"EURJPY", CurrencyPair::EURJPY},
+                                                                                 {"EURUSD", CurrencyPair::EURUSD},
+                                                                                 {"AUDCHF", CurrencyPair::AUDCHF},
+                                                                                 {"AUDJPY", CurrencyPair::AUDJPY},
+                                                                                 {"AUDNZD", CurrencyPair::AUDNZD},
+                                                                                 {"AUDUSD", CurrencyPair::AUDUSD},
+                                                                                 {"CADJPY", CurrencyPair::CADJPY},
+                                                                                 {"CHFJPY", CurrencyPair::CHFJPY},
+                                                                                 {"EURAUD", CurrencyPair::EURAUD},
+                                                                                 {"EURCAD", CurrencyPair::EURCAD},
+                                                                                 {"EURCHF", CurrencyPair::EURCHF},
+                                                                                 {"EURGBP", CurrencyPair::EURGBP},
+                                                                                 {"EURNZD", CurrencyPair::EURNZD},
+                                                                                 {"GBPAUD", CurrencyPair::GBPAUD},
+                                                                                 {"GBPCHF", CurrencyPair::GBPCHF},
+                                                                                 {"GBPJPY", CurrencyPair::GBPJPY},
+                                                                                 {"GBPNZD", CurrencyPair::GBPNZD},
+                                                                                 {"GBPUSD", CurrencyPair::GBPUSD},
+                                                                                 {"NZDJPY", CurrencyPair::NZDJPY},
+                                                                                 {"NZDUSD", CurrencyPair::NZDUSD},
+                                                                                 {"USDCAD", CurrencyPair::USDCAD},
+                                                                                 {"USDCHF", CurrencyPair::USDCHF},
+                                                                                 {"ZARJPY", CurrencyPair::ZARJPY}}};
 
-const std::unordered_map<std::string, CryptoCurrencyPair> crypto_pair_map = {
-    {"BTCJPY", CryptoCurrencyPair::BTCJPY},
-    {"BTCUSD", CryptoCurrencyPair::BTCUSD},
-    {"ETHJPY", CryptoCurrencyPair::ETHJPY},
-    {"ETHUSD", CryptoCurrencyPair::ETHUSD},
-    {"XRPJPY", CryptoCurrencyPair::XRPJPY},
-    {"XRPUSD", CryptoCurrencyPair::XRPUSD},
-    {"LTCJPY", CryptoCurrencyPair::LTCJPY},
-    {"LTCUSD", CryptoCurrencyPair::LTCUSD},
-    {"XEMJPY", CryptoCurrencyPair::XEMJPY},
-    {"XEMUSD", CryptoCurrencyPair::XEMUSD},
-    {"ADAUSD", CryptoCurrencyPair::ADAUSD},
-    {"BNBBTC", CryptoCurrencyPair::BNBBTC},
-    {"BNBUSDT", CryptoCurrencyPair::BNBUSDT}};
+const std::array<std::pair<std::string, CryptoCurrencyPair>, 13> crypto_pair_map = {{{"BTCJPY", CryptoCurrencyPair::BTCJPY},
+                                                                                     {"BTCUSD", CryptoCurrencyPair::BTCUSD},
+                                                                                     {"ETHJPY", CryptoCurrencyPair::ETHJPY},
+                                                                                     {"ETHUSD", CryptoCurrencyPair::ETHUSD},
+                                                                                     {"XRPJPY", CryptoCurrencyPair::XRPJPY},
+                                                                                     {"XRPUSD", CryptoCurrencyPair::XRPUSD},
+                                                                                     {"LTCJPY", CryptoCurrencyPair::LTCJPY},
+                                                                                     {"LTCUSD", CryptoCurrencyPair::LTCUSD},
+                                                                                     {"XEMJPY", CryptoCurrencyPair::XEMJPY},
+                                                                                     {"XEMUSD", CryptoCurrencyPair::XEMUSD},
+                                                                                     {"ADAUSD", CryptoCurrencyPair::ADAUSD},
+                                                                                     {"BNBBTC", CryptoCurrencyPair::BNBBTC},
+                                                                                     {"BNBUSDT", CryptoCurrencyPair::BNBUSDT}}};
+
+std::array<std::pair<std::string, CurrencyPair>, 24>::const_iterator getCurrencyPair(std::string key)
+{
+  auto it = std::find_if(currency_pair_map.begin(), currency_pair_map.end(), [&key](const auto &pair)
+                         { return pair.first == key; });
+  return it;
+}
+
+std::array<std::pair<std::string, CryptoCurrencyPair>, 13>::const_iterator getCryptoCurrencyPair(std::string key)
+{
+  auto it = std::find_if(crypto_pair_map.begin(), crypto_pair_map.end(), [&key](const auto &pair)
+                         { return pair.first == key; });
+  return it;
+}
 
 bool Currency::isForwardCurrencyPair(std::string prev, std::string next)
 {
   auto pair = prev + next;
-  return pair.length() == 6 && currency_pair_map.find(pair) != currency_pair_map.end();
+  return pair.length() == 6 && getCurrencyPair(pair) != currency_pair_map.end();
 }
 
 bool Currency::isReverseCurrencyPair(std::string prev, std::string next)
 {
   auto pair = next + prev;
-  return pair.length() == 6 && currency_pair_map.find(pair) != currency_pair_map.end();
+  return pair.length() == 6 && getCurrencyPair(pair) != currency_pair_map.end();
 }
 
 bool Currency::isForwardCryptoPair(std::string prev, std::string next)
 {
   auto pair = prev + next;
-  return crypto_pair_map.find(pair) != crypto_pair_map.end();
+  return getCryptoCurrencyPair(pair) != crypto_pair_map.end();
 }
 
 bool Currency::isReverseCryptoPair(std::string prev, std::string next)
 {
   auto pair = next + prev;
-  return crypto_pair_map.find(pair) != crypto_pair_map.end();
+  return getCryptoCurrencyPair(pair) != crypto_pair_map.end();
 }
 
 std::string Currency::toPairString(std::string prev, std::string next)
